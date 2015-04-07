971c6da54c4315afd4bb42b1b2430eee25fcd1bf
= <alexermashev@gmail.com>
2015-04-07 14:22:28 +0600

    add mising routes for API

--- api/init.php
+++ api/init.php
@@ -2,3 +2,6 @@
 
 $eventHandler = new MAILBOX_CLASS_EventHandler();
 $eventHandler->genericInit();
+
+OW::getRouter()->addRoute(new OW_Route('mailbox_chat_conversation', 'messages/chat/:userId', 'MAILBOX_MCTRL_Messages', 'chatConversation'));
+OW::getRouter()->addRoute(new OW_Route('mailbox_mail_conversation', 'messages/mail/:convId', 'MAILBOX_MCTRL_Messages', 'mailConversation'));

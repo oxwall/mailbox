8cc359bb50d050512ab8d62d3be9bcab15c187b6
= <alexermashev@gmail.com>
2015-04-28 12:21:55 +0600

    fix unread messages list

---
+++ update/8726/update.php
@@ -0,0 +1,37 @@
+<?php
+
+$sql = array(
+    // fix for recipients
+    'UPDATE 
+            `' . OW_DB_PREFIX . 'mailbox_conversation` 
+     SET 
+        `interlocutorDeletedTimestamp` = UNIX_TIMESTAMP(), 
+        `interlocutorAffected` = 1 
+     WHERE 
+        `deleted` = 2 
+            AND 
+         `interlocutorDeletedTimestamp` = 0',
+
+    // fix for senders
+    'UPDATE 
+        `' . OW_DB_PREFIX . 'mailbox_conversation` 
+     SET 
+        `initiatorDeletedTimestamp` = UNIX_TIMESTAMP(), 
+        `initiatorAffected` = 1 
+     WHERE 
+        `deleted` = 1 
+            AND 
+        `initiatorDeletedTimestamp` = 0'
+);
+
+foreach ( $sql as $query )
+{
+    try
+    {
+        Updater::getDbo()->query($query);
+    }
+    catch ( Exception $e )
+    {
+        Updater::getLogger()->addEntry(json_encode($e));
+    }
+}
